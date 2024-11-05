4e8199e330f47b413177341fe84d994d5c59335d
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-05 21:04:13 +0530

    fix: fix code

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,24 @@ export default {
   ],
   theme: {
   	extend: {
+      animation: {
+        fadeIn: 'fadeIn 0.3s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-  		colors: {}
+  		colors: {
+        'custom-left': '#01A273',
+        'custom-right': '#04D178',
+      }
   	}
   },
   plugins: [require("tailwindcss-animate")],
