67ebc06548925451e3d8bff9a7737753821ea467
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 02:11:48 +0530

    feat: add latest news component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import { HeroDesc, HeroTabs } from "@/components"
+import LatestNews from "@/components/home/latestNews/LatestNews"
 
 
 function Home() {
   return (
-    <div className="flex justify-around items-start bg-red-600 py-10">
+    <div className="flex justify-around items-start flex-wrap gap-6 py-10 px-5 md:px-4">
       <HeroDesc/>
       <HeroTabs/>
+      <LatestNews/>
     </div>
   )
 }
