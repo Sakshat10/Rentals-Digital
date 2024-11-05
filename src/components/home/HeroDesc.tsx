f0cd354aa11447a8b33e6980dfb7ecaa9f5fc891
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 02:09:02 +0530

    fix: did fixes

--- src/components/home/HeroDesc.tsx
+++ src/components/home/HeroDesc.tsx
@@ -1,14 +1,33 @@
-
+import HeroData from "@/data/HeroData"; 
+import Button from "../shared/Button";
 
 function HeroDesc() {
   return (
-    <div className="flex flex-col  w-[30vw] gap-5">
-    <h1 className="text-4xl text-white font-bold">Rentals Digital</h1>
-    <p className="">The new cryptocurrency payment ecosystem that rewards its users for their travels and tourism.</p>  
-    <button className="px-5 py-3 border-2 w-fit bg-slate-100 shadow-lg text-text-color font-semibold">Buy Token Now</button>
-
+    <div className="flex flex-col w-full md:w-[45vw] lg:w-[40vw] gap-6 text-white">
+      <h1 className="lg:text-7xl text-5xl font-bold">Rentals Digital</h1>
+      <p className="md:w-[33vw] text-justify">
+        The new cryptocurrency payment ecosystem that rewards its users for
+        their travels and tourism.
+      </p>
+      <Button>Buy Token Now</Button>
+      <div className="flex items-center gap-4">
+        <p className=" text-xl">Accepting:</p>
+        <div className="flex gap-2">
+          {HeroData.Icons.map((iconData, index) => (
+            <a href={iconData.href} key={index} className="text-white">
+              <iconData.icon />
+            </a>
+          ))}
+        </div>
+      </div>
+      <p>Referral will paid automatically in the purchase currency</p>
+      <div className="flex items-center gap-4 text-white">
+      <p className="font-semibold text-lg"><span className="text-black">5% </span> in phase 1</p>
+      <div className="h-5 border-l border-white"></div>
+      <p className="font-semibold text-lg"><span className="text-black">8% </span> in phase 2</p>
     </div>
-  )
+    </div>
+  );
 }
 
-export default HeroDesc
+export default HeroDesc;
