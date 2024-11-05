273c619b591cb3857b0ad41028c66deab7e696ba
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-05 21:04:34 +0530

    feat: add navbar and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Footer, Navbar } from "./components"
 import { Home } from "./pages"
 
 function App() {
   return (
-    <div>
+    <div className="bg-gradient-to-r from-custom-left to-custom-right">
+      <Navbar/>
       <Home/>
+      <Footer/>
     </div>
   )
 }
