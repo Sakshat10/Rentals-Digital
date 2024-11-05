a001419f00b91290348b519fc35251518d6265cf
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-05 21:05:49 +0530

    feat: add nav data

---
+++ src/data/Navbar.ts
@@ -0,0 +1,31 @@
+type NavbarItem = {
+    name: string;
+    link: string;
+  };
+  
+
+  const Navbar: NavbarItem[] = [
+    {
+      name: "About Rental",
+      link: "/about",
+    },
+    {
+      name: "Platforms",
+      link: "/platforms",
+    },
+    {
+      name: "Acquisition",
+      link: "/acquisition",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+    {
+      name: "Whitepaper",
+      link: "/whitepaper",
+    },
+  ];
+  
+  export default Navbar;
+  
