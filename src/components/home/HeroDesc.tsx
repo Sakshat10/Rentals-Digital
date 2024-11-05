9c31c60fc7d33a29b4a2b9155c885f990256ae43
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 00:19:18 +0530

    feat: add hero description

---
+++ src/components/home/HeroDesc.tsx
@@ -0,0 +1,14 @@
+
+
+function HeroDesc() {
+  return (
+    <div className="flex flex-col  w-[30vw] gap-5">
+    <h1 className="text-4xl text-white font-bold">Rentals Digital</h1>
+    <p className="">The new cryptocurrency payment ecosystem that rewards its users for their travels and tourism.</p>  
+    <button className="px-5 py-3 border-2 w-fit bg-slate-100 shadow-lg text-text-color font-semibold">Buy Token Now</button>
+
+    </div>
+  )
+}
+
+export default HeroDesc
