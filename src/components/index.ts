b35d8fb03de6757aff75b13a5ef93981330d31a2
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 00:19:09 +0530

    feat: add imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export {default as Navbar} from "./shared/Navbar"
 export {default as Footer} from "./shared/Footer"
+export {default as HeroDesc} from "./home/HeroDesc"
+export {default as HeroTabs} from "./home/HeroTabs"
