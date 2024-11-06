25758ff9172f47b417713654c0980cb083c8aa43
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 23:32:44 +0530

    feat: add purchase and referal component

---
+++ src/components/home/hero/heroTabsSection/Referral.tsx
@@ -0,0 +1,34 @@
+import Button from "@/components/shared/Button"
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+function Referral() {
+  return (
+    <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Referral</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="username">Referal ID</Label>
+              <Input id="username" defaultValue="random123.rentals" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full border-2 border-custom-left">connect wallet</Button>
+          </CardFooter>
+        </Card>
+    </div>
+  )
+}
+
+export default Referral
---
+++ src/components/home/hero/heroTabsSection/TokenPurchase.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Button from "@/components/shared/Button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+function TokenPurchase() {
+  const [isConnected, setIsConnected] = useState(false);
+
+  const handleButtonClick = () => {
+    if (!isConnected) {
+
+      setIsConnected(true);
+    } else {
+      console.log("Proceeding with token purchase...");
+    }
+  };
+
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Token Purchase</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <div className="space-y-1">
+            <Label htmlFor="amount">Enter the amount</Label>
+            <Input id="amount" defaultValue="0.00" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button
+            onClick={handleButtonClick}
+            className={`w-full border-2 ${
+              isConnected ? "bg-[#01A273] text-white" : "border-custom-left"
+            }`}
+          >
+            {isConnected ? "Buy Now" : "Connect Wallet"}
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export default TokenPurchase;
