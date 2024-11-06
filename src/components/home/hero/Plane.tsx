44a8a215611666c63c122141eef546d35262760b
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 12:34:42 +0530

    feat: add plane animation

---
+++ src/components/home/hero/Plane.tsx
@@ -0,0 +1,12 @@
+
+import plane from "@/assets/Images/plane.png"
+
+function Plane() {
+  return (
+    <div className="absolute animate-float top-1/2">
+      <img src={plane} alt="" />
+    </div>
+  )
+}
+
+export default Plane;
