31629135921d7a7daa9b3f1c9805480d315a5623
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 23:34:55 +0530

    feat: add platform section

---
+++ src/pages/Platform.tsx
@@ -0,0 +1,12 @@
+import PlatformSection from "@/components/platform/PlatformSection"
+
+
+function Platform() {
+  return (
+    <div className="">
+      <PlatformSection/>
+    </div>
+  )
+}
+
+export default Platform
