091156e4e7ad6699ee7135683c5ac7066f0c873b
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 12:36:07 +0530

    feat: add components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,22 @@
 import { HeroDesc, HeroTabs } from "@/components"
+import Ballon from "@/components/home/hero/Ballon"
+import Plane from "@/components/home/hero/Plane"
 import LatestNews from "@/components/home/latestNews/LatestNews"
+import PartnersMarquee from "@/components/home/partners/Partners"
 
 
 function Home() {
   return (
-    <div className="flex justify-around items-start flex-wrap gap-6 py-10 px-5 md:px-4">
+    <div className="">
+      <div className="lg:h-[90vh] relative">
+      <div className=" flex justify-around items-start flex-wrap gap-6 py-10 px-5 md:px-4">
       <HeroDesc/>
       <HeroTabs/>
+      </div>
+      <Plane/>
+      <Ballon/>
+      </div>
+      <PartnersMarquee/>
       <LatestNews/>
     </div>
   )
