aa903dde2928d05b5f4c3a10a32955e474267188
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 23:35:06 +0530

    fix: did fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,12 @@
-import { HeroDesc, HeroTabs } from "@/components"
-import Ballon from "@/components/home/hero/Ballon"
-import Plane from "@/components/home/hero/Plane"
-import LatestNews from "@/components/home/latestNews/LatestNews"
-import PartnersMarquee from "@/components/home/partners/Partners"
+import { Hero, LatestNews, Partners, ProjectEcosystem } from "@/components"
 
 
 function Home() {
   return (
     <div className="">
-      <div className="lg:h-[90vh] relative">
-      <div className=" flex justify-around items-start flex-wrap gap-6 py-10 px-5 md:px-4">
-      <HeroDesc/>
-      <HeroTabs/>
-      </div>
-      <Plane/>
-      <Ballon/>
-      </div>
-      <PartnersMarquee/>
+      <Hero/>
+      <ProjectEcosystem/>
+      <Partners/>
       <LatestNews/>
     </div>
   )
