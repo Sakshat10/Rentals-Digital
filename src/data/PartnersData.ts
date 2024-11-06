164185bef581e04755e2085e0cbba80deacbcc52
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 12:35:52 +0530

    feat: add data for different components

---
+++ src/data/PartnersData.ts
@@ -0,0 +1,28 @@
+export const partners = [
+    {
+      name: "google 1",
+      image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+      link: "#",
+    },
+    {
+      name: "google 2",
+      image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+      link: "#",
+    },
+    {
+      name: "google 3",
+      image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+      link: "#",
+    },
+    {
+      name: "google 4",
+      image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+      link: "#",
+    },
+    {
+      name: "google 5",
+      image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+      link: "#",
+    },
+  ];
+  
