a161d11c55dcf9e42acd296371b30d4b58576c5b
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 12:33:28 +0530

    feat: add custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,13 @@ export default {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
         },
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-30px)' },
+        },
+      },
+       animation: {
+        float: 'float 8s ease-in-out infinite',
       },
   		borderRadius: {
   			lg: 'var(--radius)',
