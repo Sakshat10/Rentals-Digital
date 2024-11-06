9e60a42f6c226c644ad42b0efcb30d8e3f491418
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 23:33:56 +0530

    feat: add json data

---
+++ src/data/EcosystemSmData.tsx
@@ -0,0 +1,47 @@
+import ecoSmallImage from "@/assets/Images/ecoSmallImage.png";
+
+const featuresSmData = [
+  {
+    id: "right",
+    mainClassName: "flex items-center justify-start gap-5 pt-7",
+    secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+    image: ecoSmallImage,
+    data: {
+      title: "Instant Referral Bonuses",
+      description: "Earn on referrals right at purchase.",
+    },
+  },
+  {
+    id: "right",
+    mainClassName: "flex items-center justify-start gap-5 pt-7",
+    secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+    image: ecoSmallImage,
+    data: {
+      title: "Instant Referral Bonuses",
+      description: "Earn on referrals right at purchase.",
+    },
+  },
+  {
+    id: "right",
+    mainClassName: "flex items-center justify-start gap-5 pt-7",
+    secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+    image: ecoSmallImage,
+    data: {
+      title: "Instant Referral Bonuses",
+      description: "Earn on referrals right at purchase.",
+    },
+  },
+  {
+    id: "right",
+    mainClassName: "flex items-center justify-start gap-5 pt-7",
+    secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+    image: ecoSmallImage,
+    data: {
+      title: "Instant Referral Bonuses",
+      description: "Earn on referrals right at purchase.",
+    },
+  },
+  
+];
+
+export default featuresSmData;
