7309a0f307b1a68f0a6982eb007569df72a9a0e1
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-06 23:33:10 +0530

    fix: did fixes

--- src/components/home/partners/Partners.tsx
+++ src/components/home/partners/Partners.tsx
@@ -1,8 +1,7 @@
-import React from "react";
 import Marquee from "react-fast-marquee";
 import { partners } from "@/data/PartnersData";
 
-const Partners: React.FC = () => {
+function Partners(){
   return (
     <div className="py-8 overflow-x-hidden">
       <h2 className="text-center text-white text-3xl font-bold mb-6">Our Partners</h2>
@@ -10,8 +9,7 @@ const Partners: React.FC = () => {
         <div className="flex gap-28 items-center">
           {partners.map((partner, index) => (
             <a href={partner.link} key={index} className="flex items-center space-x-3">
-              <img src={partner.image} alt={partner.name} className="w-20 h-20 object-contain" />
-              <span className="text-white text-lg font-medium">{partner.name}</span>
+              <img src={partner.image} alt={partner.name} className="w-44 h-32 object-contain" />
             </a>
           ))}
         </div>
