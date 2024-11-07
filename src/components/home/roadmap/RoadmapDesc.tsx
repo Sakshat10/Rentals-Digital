e16233b2f83c74fc9d882fcb82122fa47df646d2
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-07 10:48:20 +0530

    feat: add roadmap description

---
+++ src/components/home/roadmap/RoadmapDesc.tsx
@@ -0,0 +1,24 @@
+import Roadmap from "@/data/RoadmapData";
+
+
+function RoadmapDesc() {
+  return (
+    <div className="w-full h-full">
+      {Roadmap.map((item, index) => (
+        <div key={index} className={item.mainClassname}>
+          <div className="flex items-center gap-5">
+            {item.id === "right" && <img src={item.image} alt={`${item.title} image`} width={200} height={20} />}
+            <div className="flex flex-col items-start w-[20vw] gap-3">
+              <p>{item.quarter}</p>
+              <h1 className="text-white font-bold">{item.title}</h1>
+              <p className="text-slate-300">{item.description}</p>
+            </div>
+            {item.id === "left" && <img src={item.image} alt={`${item.title} image`} width={200} height={20} />}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default RoadmapDesc;
