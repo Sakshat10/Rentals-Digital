81e2b1433ab36e6ad64dc74a2d225a5194647556
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-07 10:48:29 +0530

    feat: add roadmap plane image

---
+++ src/components/home/roadmap/RoadmapPlane.tsx
@@ -0,0 +1,11 @@
+import fullPlane from "@/assets/Images/fullPlane.png"
+
+function RoadmapPlane() {
+  return (
+    <div className="">
+      <img src={fullPlane} alt="" className="lg:w-[35vw] animate-move"/>
+    </div>
+  )
+}
+
+export default RoadmapPlane
