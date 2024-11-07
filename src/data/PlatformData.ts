7be3917ac7b90d10f512f680c9439055bd28e9e6
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-07 10:49:35 +0530

    feat: add reward section in platform

---
+++ src/data/PlatformData.ts
@@ -0,0 +1,23 @@
+import Ribbon from "@/assets/Images/Ribbon.png"
+import Star from "@/assets/Images/Star.png"
+import wallet from "@/assets/Images/wallet.png"
+
+const platformData = [
+    {
+      "icon": Ribbon,
+      "title": "Earn Rewards on Every Journey",
+      "description": "Earn rewards every time you travel."
+    },
+    {
+      "icon": Star,
+      "title": "Seamless Crypto Payments",
+      "description": "Easily make crypto payments for your transactions."
+    },
+    {
+      "icon": wallet,
+      "title": "Exclusive Referral Bonuses",
+      "description": "Get exclusive bonuses when you refer friends."
+    }
+  ]
+
+export default platformData;
