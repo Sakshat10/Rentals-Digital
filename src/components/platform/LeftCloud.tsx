0a55507bf776c36385d33edbb50a723559224879
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-08 01:05:30 +0530

    fix: did fixes

--- src/components/platform/LeftCloud.tsx
+++ src/components/platform/LeftCloud.tsx
@@ -2,7 +2,7 @@ import sideCloud from "@/assets/Images/sharedImg/leftSideCloud.png"
 
 function LeftCloud() {
   return (
-    <div className="absolute top-[40vh] left-0">
+    <div className="absolute top-[40vh] left-0 hidden lg:block">
       <img src={sideCloud} alt="" className="w-14"/>
     </div>
   )
