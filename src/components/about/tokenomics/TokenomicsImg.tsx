d1b51629371f744d682b2646f4ad928aa16e1606
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-14 11:30:20 +0530

    fix: did fix in img

--- src/components/about/tokenomics/TokenomicsImg.tsx
+++ src/components/about/tokenomics/TokenomicsImg.tsx
@@ -1,9 +1,9 @@
-import tokenomics from "@/assets/Images/AboutImg/Tokenomics.webp"
+import tokenomics from "@/assets/Images/AboutImg/Tokenomics.svg"
 
 function TokenomicsImg() {
   return (
     <div className="flex items-center justify-center pt-20 pl-10">
-      <img src={tokenomics} alt=""/>
+      <img src={tokenomics} alt="" className="w-4/5"/>
     </div>
   )
 }
