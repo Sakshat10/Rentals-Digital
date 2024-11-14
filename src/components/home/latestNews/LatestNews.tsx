a18706fe2cbe6d0904ba586c6e8ae77569095f45
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-14 22:32:25 +0530

    feat: add dynamic routing

--- src/components/home/latestNews/LatestNews.tsx
+++ src/components/home/latestNews/LatestNews.tsx
@@ -1,17 +1,17 @@
-
 import NewsCard from './NewsCard';
 import newsData from '@/data/NewsData';
 
-function LatestNews(){
+function LatestNews() {
   return (
     <section className="py-16 px-4">
       <div className="text-center mb-10">
-        <h2 className="text-center text-5xl font-bold text-white">Latest News</h2>
+        <h2 className="text-5xl font-bold text-white">Latest News</h2>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
         {newsData.map((news) => (
           <NewsCard
             key={news.id}
+            id={news.id}
             title={news.title}
             date={news.date}
             readTime={news.readTime}
@@ -22,6 +22,6 @@ function LatestNews(){
       </div>
     </section>
   );
-};
+}
 
 export default LatestNews;
--- src/components/home/latestNews/NewsCard.tsx
+++ src/components/home/latestNews/NewsCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { FiCalendar, FiClock } from "react-icons/fi";
-
+import { FiCalendar, FiClock } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 interface NewsCardProps {
+  id: number;
   title: string;
   date: string;
   readTime: string;
@@ -10,22 +11,24 @@ interface NewsCardProps {
   image: string;
 }
 
-const NewsCard: React.FC<NewsCardProps> = ({ title, date, readTime, description, image }) => {
+const NewsCard: React.FC<NewsCardProps> = ({ id, title, date, readTime, description, image }) => {
   return (
-    <div className="p-6 space-y-4 max-w-sm">
+    <div className="p-6 space-y-4 max-w-sm  rounded-md">
+       <Link to={`/news/${id}`}>
       <img src={image} alt="News" className="w-full h-56 object-cover rounded-md shadow-md" />
+       </Link>
       <div className="flex items-center text-white space-x-2">
-      <FiCalendar className="text-sm" />
-      <span className="text-sm">{date}</span>
-      <span>|</span>
-      <FiClock className="text-sm" />
-      <span className="text-sm">{readTime}</span>
-    </div>
+        <FiCalendar className="text-sm" />
+        <span className="text-sm">{date}</span>
+        <span>|</span>
+        <FiClock className="text-sm" />
+        <span className="text-sm">{readTime}</span>
+      </div>
       <h3 className="text-lg font-semibold text-white">{title}</h3>
-      <p className="text-white ">{description}</p>
-      <a href="#" className="text-[#252A297A] font-semibold flex items-center hover:underline">
+      <p className="text-white">{description}</p>
+      <Link to={`/news/${id}`} className="text-[#252A297A] font-semibold flex items-center hover:underline">
         Read more <span className="ml-2">→</span>
-      </a>
+      </Link>
     </div>
   );
 };
