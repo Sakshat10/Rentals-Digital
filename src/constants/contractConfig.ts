adf60fe3632317f7b10730b0c3f3edaa048b8432
Umar <khanumar0108@gmail.com>
2024-11-20 11:11:25 +0530

    feat: Transfer contract to mainnet

--- src/constants/contractConfig.ts
+++ src/constants/contractConfig.ts
@@ -2,8 +2,8 @@ import { Abi } from "viem";
 import { RENTAL_CONTRACT_ABI, USDT_CONTRACT_ABI } from "./abi";
 
 export const contractAddress = {
-	rentalAddress: "0x16D0630DEE7562C9f3cAB7b37779ab9522C9eAab" as `0x${string}`,
-	usdtAddress: "0x1Ba1c8C5E4F2CB9cb2Ee116BeB5f185C59523A35" as `0x${string}`,
+	rentalAddress: "0xBc06f43f5BcE98cd9f888E79EF8cdE848B63Fa48" as `0x${string}`,
+	usdtAddress: "0x55d398326f99059fF775485246999027B3197955" as `0x${string}`,
 };
 
 const contractConfig: {
