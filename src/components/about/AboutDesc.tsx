6403dd6009e4dc8f5ef7a090cbe1f9fee323609d
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-20 11:01:22 +0530

    fix: did fixes in button navigation

--- src/components/about/AboutDesc.tsx
+++ src/components/about/AboutDesc.tsx
@@ -7,7 +7,9 @@ function AboutDesc() {
       <h1 className="md:text-7xl text-4xl  font-bold">About Rentals</h1>
       <p className="text-justify">Rentals is a revolutionary platform that integrates all aspects of the travel experience into a single ecosystem. </p>
       <p className="text-justify">Leveraging blockchain technology and cryptocurrencies, Rentals streamlines the reservation and payment process, ensures transparency through genuine customer reviews, and rewards users for their activities and interactions on the platform.</p>
+      <a href="/">
       <Button>Buy Token Now</Button>
+      </a>
     </div>
   )
 }
