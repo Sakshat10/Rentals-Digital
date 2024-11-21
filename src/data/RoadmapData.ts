455ecb2c02a1fab468dff3b9b30f6ccdd7ef33d8
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-21 11:30:00 +0530

    fix: fix roadmap data for sm and md

--- src/data/RoadmapData.ts
+++ src/data/RoadmapData.ts
@@ -3,43 +3,55 @@ import dottedLine from "@/assets/Images/homeImages/dottedLine.png";
 const Roadmap = [
   {
     id: "left",
-    quarter: "2024 - Q2",
-    title: "Referral Boost & Payment Integration",
-    description: "Increase referral reward to 8% in Phase 2; enable PIx payment options.",
+    quarter: "2025 - Q2",
+    title: "Launch of the Rentals Digital Platform",
+    description: "Distribution of earnings to token holders",
+    description2: "closing deals with brand ambassadors",
     image: dottedLine,
-    mainClassname: "title absolute top-[20vh] left-[5vw] flex flex-col gap-10"
+    mainClassname: "title absolute top-[15vh] left-[5vw] flex flex-col gap-10"
   },
   {
     id: "left",
     quarter: "2024 - Q4",
-    title: "Advanced Analytics",
-    description: "Implement enhanced tracking and analytics for user engagement.",
+    title: "Creation of the website and communication channels",
+    description: "Smart contract development",
+    description2: "Marketing Planning for the public token offering",
+    description3: "Start of platform development",
+    description4: "Launch of the public token offering",
     image: dottedLine,
     mainClassname: "title absolute top-[60vh] left-[5vw] flex flex-col gap-10"
   },
   {
     id: "right",
-    quarter: "2024 - Q1",
-    title: "New Partnerships",
-    description: "Collaborate with global partners to expand reach and services.",
+    quarter: "2025 - Q1",
+    title: "Listing of Rents on DEX and CEX",
+    description: "Closed beta testing of the platform",
+    description2: "Platform security testing",
+    description3: "Audit",
+    description4: "Marketing campaign to expand reach",
+    description5: "Securing strategic partnership",
     image: dottedLine,
-    mainClassname: "title absolute top-[15vh] right-[2vw] flex flex-col gap-10"
+    mainClassname: "title absolute top-[10vh] right-[2vw] flex flex-col gap-10"
   },
   {
     id: "right",
-    quarter: "2025 - Q3",
-    title: "App Launch",
-    description: "Release mobile application with optimized features for user convenience.",
+    quarter: "2024 - Q3",
+    title: "Market research",
+    description: "Concept development",
+    description2: "Feasibility study",
+    description3: "Creation of the economy and tokenomics",
+    description4: "Development of project documentation and whitepaper",
     image: dottedLine,
-    mainClassname: "title absolute top-[40vh] right-[2vw] flex flex-col gap-10"
+    mainClassname: "title absolute top-[46vh] right-[2vw] flex flex-col gap-10"
   },
   {
     id: "right",
     quarter: "2025 - Q5",
-    title: "Expansion to New Markets",
-    description: "Enter new geographical markets to broaden user base and offerings.",
+    title: "Listing on additional CEX platforms",
+    description: "Creation of a strategic growth team",
+    description2: "Addition of new funcitonalities Continuous improvement",
     image: dottedLine,
-    mainClassname: "title absolute top-[75vh] right-[2vw] flex flex-col gap-10"
+    mainClassname: "title absolute top-[80vh] right-[2vw] flex flex-col gap-10"
   }
 ];
 
