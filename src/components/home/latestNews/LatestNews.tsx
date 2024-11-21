2baef9b59453162dc7ad938f708946f31cf80f68
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-22 00:31:14 +0530

    fix: fix news card

--- src/components/home/latestNews/LatestNews.tsx
+++ src/components/home/latestNews/LatestNews.tsx
@@ -7,7 +7,7 @@ function LatestNews() {
       <div className="text-center mb-10">
         <h2 className="text-5xl font-bold text-white">Latest News</h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8  items-stretch">
         {newsData.map((news) => (
           <NewsCard
             key={news.id}
--- src/components/home/latestNews/NewsCard.tsx
+++ src/components/home/latestNews/NewsCard.tsx
@@ -3,13 +3,13 @@ import { FiCalendar, FiClock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 // Import shared type
-import  NewsItem  from '@/data/NewsData';
+import NewsItem from '@/data/NewsData';
 
 type NewsCardProps = Pick<NewsItem, 'id' | 'title' | 'date' | 'readTime' | 'description' | 'image'>;
 
 const NewsCard: React.FC<NewsCardProps> = ({ id, title, date, readTime, description, image }) => {
   return (
-    <div className="p-6 space-y-4 max-w-sm rounded-md">
+    <div className="p-6 space-y-4 max-w-sm rounded-md  h-full flex flex-col">
       <Link to={`/news/${id}`}>
         <img src={image} alt={title} className="w-full h-56 object-cover rounded-md shadow-xl" />
       </Link>
@@ -21,7 +21,7 @@ const NewsCard: React.FC<NewsCardProps> = ({ id, title, date, readTime, descript
         <span className="text-sm">{readTime}</span>
       </div>
       <h3 className="text-lg font-semibold text-white">{title}</h3>
-      <p className="text-white">{description}</p>
+      <p className="text-white flex-grow">{description}</p>
       <Link to={`/news/${id}`} className="text-[#252A297A] font-semibold flex items-center hover:underline">
         Read more <span className="ml-2">→</span>
       </Link>
