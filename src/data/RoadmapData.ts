394003c3d0b97b7e525d63e560b90be057355b87
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-21 15:16:50 +0530

    fix: did fixes

--- src/data/RoadmapData.ts
+++ src/data/RoadmapData.ts
@@ -2,7 +2,7 @@ import dottedLine from "@/assets/Images/homeImages/dottedLine.png";
 
 const Roadmap = [
   {
-    id: "left",
+    id: "right",
     quarter: "2024 - Q4",
     title: "Creation of the website and communication channels",
     description: "Smart contract development",
@@ -10,19 +10,19 @@ const Roadmap = [
     description3: "Start of platform development",
     description4: "Launch of the public token offering",
     image: dottedLine,
-    mainClassname: "title absolute top-[15vh] left-[5vw] flex flex-col gap-10"
+    mainClassname: "title absolute top-[17vh] right-[2vw] flex flex-col gap-10 "
   },
   {
-    id: "left",
+    id: "right",
     quarter: "2025 - Q2",
     title: "Launch of the Rentals Digital Platform",
     description: "Distribution of earnings to token holders",
     description2: "closing deals with brand ambassadors",
     image: dottedLine,
-    mainClassname: "title absolute top-[60vh] left-[5vw] flex flex-col gap-10"
+    mainClassname: " title absolute top-[65vh] right-[2vw] flex flex-col gap-10"
   },
   {
-    id: "right",
+    id: "left",
     quarter: "2025 - Q1",
     title: "Listing of Rents on DEX and CEX",
     description: "Closed beta testing of the platform",
@@ -31,10 +31,10 @@ const Roadmap = [
     description4: "Marketing campaign to expand reach",
     description5: "Securing strategic partnership",
     image: dottedLine,
-    mainClassname: "title absolute top-[46vh] right-[2vw] flex flex-col gap-10 "
+    mainClassname: "title absolute top-[44vh] left-[5vw] flex flex-col gap-10 "
   },
   {
-    id: "right",
+    id: "left",
     quarter: "2024 - Q3",
     title: "Market research",
     description: "Concept development",
@@ -42,16 +42,16 @@ const Roadmap = [
     description3: "Creation of the economy and tokenomics",
     description4: "Development of project documentation and whitepaper",
     image: dottedLine,
-    mainClassname: "title absolute top-[10vh] right-[2vw] flex flex-col gap-10"
+    mainClassname: "title absolute top-[8vh] left-[5vw] flex flex-col gap-10"
   },
   {
-    id: "right",
+    id: "left",
     quarter: "2025 - Q3",
     title: "Listing on additional CEX platforms",
     description: "Creation of a strategic growth team",
     description2: "Addition of new funcitonalities Continuous improvement",
     image: dottedLine,
-    mainClassname: "title absolute top-[80vh] right-[2vw] flex flex-col gap-10"
+    mainClassname: "title absolute top-[79vh] left-[5vw] flex flex-col gap-10"
   }
 ];
 
