45ddbd30b0b6158818fcd2ca8835a0a1e63c5182
Umar <khanumar0108@gmail.com>
2024-11-23 22:20:15 +0530

    feat: Add whitepaper link to learn more button

--- src/components/home/ecosystem/ProjectSection.tsx
+++ src/components/home/ecosystem/ProjectSection.tsx
@@ -5,34 +5,48 @@ import Button from "@/components/shared/Button";
 import DarkCloudFull from "./DarkCloudFull";
 import SmallBallon from "./SmallBallon";
 import HalfPlane from "./HalfPlane";
+import { Link } from "react-router-dom";
 
 function ProjectSection() {
-  return (
-    <div className="py-10">
-      <h1 className="text-center text-5xl font-bold text-white">Project Ecosystem</h1>
-      
-      <div className="hidden relative lg:block">
-        <Features />
-      </div>
-      
-      <div className="flex items-center justify-center animate-float py-8 h-[75vh] ">
-        <img src={Ecosystem} alt="" className="" />
-      </div>
+	return (
+		<div className="py-10">
+			<h1 className="text-center text-5xl font-bold text-white">
+				Project Ecosystem
+			</h1>
 
-        <div className="block lg:hidden">
-        <FeaturesSm />
-      </div>
-      <div className="flex justify-center mt-8">
-      <Button>Learn more about the platform</Button>
-      </div>
+			<div className="hidden relative lg:block">
+				<Features />
+			</div>
 
-      <div className="relative">
-      <DarkCloudFull/>
-      <SmallBallon/>
-      <HalfPlane/>
-      </div>
-    </div>
-  );
+			<div className="flex items-center justify-center animate-float py-8 h-[75vh] ">
+				<img
+					src={Ecosystem}
+					alt=""
+					className=""
+				/>
+			</div>
+
+			<div className="block lg:hidden">
+				<FeaturesSm />
+			</div>
+			<div className="flex justify-center mt-8">
+				<Button>
+					<Link
+						to={"/WhitePaper/WhitePaper.pdf"}
+						target="_blank"
+					>
+						Learn more about the platform
+					</Link>
+				</Button>
+			</div>
+
+			<div className="relative">
+				<DarkCloudFull />
+				<SmallBallon />
+				<HalfPlane />
+			</div>
+		</div>
+	);
 }
 
 export default ProjectSection;
