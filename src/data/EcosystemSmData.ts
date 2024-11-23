8a554476f668a7f088b5b6cff247882a5039ff49
sakshat kumar <Sakshatkumar007@gmail.com>
2024-11-23 22:10:08 +0530

    fix: did fixes

--- src/data/EcosystemSmData.ts
+++ src/data/EcosystemSmData.ts
@@ -4,7 +4,7 @@ const featuresSmData = [
 	{
 		id: "right",
 		mainClassName: "flex items-center justify-start gap-5 pt-7",
-		secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+		secondaryClass: "flex flex-col justify-start items-start gap-3  text-start",
 		image: ecoSmallImage,
 		data: {
 			title: "Instant Referral Bonuses",
@@ -14,7 +14,7 @@ const featuresSmData = [
 	{
 		id: "right",
 		mainClassName: "flex items-center justify-start gap-5 pt-7",
-		secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+		secondaryClass: "flex flex-col justify-start items-start gap-3  text-start",
 		image: ecoSmallImage,
 		data: {
 			title: "Travel and Earn with Rentals",
@@ -24,7 +24,7 @@ const featuresSmData = [
 	{
 		id: "right",
 		mainClassName: "flex items-center justify-start gap-5 pt-7",
-		secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+		secondaryClass: "flex flex-col justify-start items-start gap-3  text-start",
 		image: ecoSmallImage,
 		data: {
 			title: "Booking and payments with just 1 click",
@@ -34,7 +34,7 @@ const featuresSmData = [
 	{
 		id: "right",
 		mainClassName: "flex items-center justify-start gap-5 pt-7",
-		secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+		secondaryClass: "flex flex-col justify-start items-start gap-3  text-start",
 		image: ecoSmallImage,
 		data: {
 			title: "Decentralized Tourism Rewards",
