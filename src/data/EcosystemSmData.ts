5c5c8b7bc9cced71e8fda5eea8406b16ef54e393
Umar <khanumar0108@gmail.com>
2024-11-23 21:35:18 +0530

    feat: Update text data for ecosystem

--- src/data/EcosystemSmData.ts
+++ src/data/EcosystemSmData.ts
@@ -1,47 +1,46 @@
 import ecoSmallImage from "@/assets/Images/homeImages/ecoSmallImage.png";
 
 const featuresSmData = [
-  {
-    id: "right",
-    mainClassName: "flex items-center justify-start gap-5 pt-7",
-    secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
-    image: ecoSmallImage,
-    data: {
-      title: "Instant Referral Bonuses",
-      description: "Earn on referrals right at purchase.",
-    },
-  },
-  {
-    id: "right",
-    mainClassName: "flex items-center justify-start gap-5 pt-7",
-    secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
-    image: ecoSmallImage,
-    data: {
-      title: "Booking and payments with just 1 click",
-      description: "Without multiple intermediaries"
-    },
-  },
-  {
-    id: "right",
-    mainClassName: "flex items-center justify-start gap-5 pt-7",
-    secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
-    image: ecoSmallImage,
-    data: {
-      title: "Instant Referral Bonuses",
-      description: "Earn on referrals right at purchase.",
-    },
-  },
-  {
-    id: "right",
-    mainClassName: "flex items-center justify-start gap-5 pt-7",
-    secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
-    image: ecoSmallImage,
-    data: {
-      title: "Instant Referral Bonuses",
-      description: "Earn on referrals right at purchase.",
-    },
-  },
-  
+	{
+		id: "right",
+		mainClassName: "flex items-center justify-start gap-5 pt-7",
+		secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+		image: ecoSmallImage,
+		data: {
+			title: "Instant Referral Bonuses",
+			description: "Earn on referrals right at purchase.",
+		},
+	},
+	{
+		id: "right",
+		mainClassName: "flex items-center justify-start gap-5 pt-7",
+		secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+		image: ecoSmallImage,
+		data: {
+			title: "Travel and Earn with Rentals",
+			description: "Rewarding crypto payments for your journeys.",
+		},
+	},
+	{
+		id: "right",
+		mainClassName: "flex items-center justify-start gap-5 pt-7",
+		secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+		image: ecoSmallImage,
+		data: {
+			title: "Booking and payments with just 1 click",
+			description: "Without multiple intermediaries.",
+		},
+	},
+	{
+		id: "right",
+		mainClassName: "flex items-center justify-start gap-5 pt-7",
+		secondaryClass: "flex flex-col justify-start items-start gap-3  text-end",
+		image: ecoSmallImage,
+		data: {
+			title: "Decentralized Tourism Rewards",
+			description: "Unlock value every time you travel",
+		},
+	},
 ];
 
 export default featuresSmData;
